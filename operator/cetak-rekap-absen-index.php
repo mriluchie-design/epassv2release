2c39a56b516edbadc1335c268f2c2d7eee2ed732
iLugroup Multimedia <mriluchie@gmail.com>
2026-06-22 08:26:02 +0800

    .
    .

--- operator/cetak-rekap-absen-index.php
+++ operator/cetak-rekap-absen-index.php
@@ -75,13 +75,13 @@ function goBack() {
 							</div>
 						</div>
 
-							<div class="tab-content" style="background:#f2f3f8;overflow:scroll;height:800px">
+							<div class="tab-content" style="background:#f2f3f8;overflow:scroll;height:auto">
 							<div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid mt-3">
 								<div class="bg-white w-50 ml-auto mr-auto">
-	<img src="../assets/img/header.png" style="width:787px; display:block; margin-left:40px;margin-right:auto;margin-bottom:40px;padding-top:0px;"/>
-						<table class="table-checkable" align="center" width="650" border="0" style="line-height:1.5; position:relative;top:-70px; font-family: Roboto; font-size: 12px;" id="kt_table_1">
+	
+						<table class="table-checkable" align="center" width="650" border="0" style="line-height:1.5; position:relative;top:100px; font-family: Roboto; font-size: 12px;" id="kt_table_1">
   <tr>
-    <td colspan="10" style="text-align: center"><strong><U>DATA REKAP ABSEN PEGAWAI<U></strong></td>
+    <td colspan="10" style="text-align: center"><strong><U>DATA REKAP ABSEN<U></strong></td>
   </tr>
   <tr>
     <td colspan="10" style="text-align: center; font-style: italic;">Periode : 01/01/2025 s/d 31/01/2025</td>
@@ -121,24 +121,24 @@ function goBack() {
     <td colspan="10">&nbsp;</td>
   </tr>
 </table>
-						<table align="center" border="1" cellpadding="5" style="width: 650px; position:relative;top:-80px; text-align:justify; line-height:1.5; font-family: Roboto; font-size: 11px;">
+						<table align="center" border="1" cellpadding="5" style="width: 650px; position:relative;top:100px; text-align:justify; line-height:1.5; font-family: Roboto; font-size: 11px;">
   <tr>
-    <td width="1%" rowspan="2" bgcolor="#f1f1f1" style="text-align:center"><b>No<b></td>
-    <td width="18%" rowspan="2" bgcolor="#f1f1f1" style="text-align:center"><b>Tanggal<b></td>
-    <td colspan="3" bgcolor="#f1f1f1" style="text-align:center"><b>Absensi<b></td>
-    <td colspan="3" bgcolor="#f1f1f1" style="text-align:center"><b>Lembur<b></td>
-    <td width="17%" rowspan="2" bgcolor="#f1f1f1" style="text-align:center"><b>Status<br>
+    <td width="1%" rowspan="2" bgcolor="#f7fcff" style="text-align:center"><b>No<b></td>
+    <td width="18%" rowspan="2" bgcolor="#f7fcff" style="text-align:center"><b>Tanggal<b></td>
+    <td colspan="3" bgcolor="#f7fcff" style="text-align:center"><b>Absensi<b></td>
+    <td colspan="3" bgcolor="#f7fcff" style="text-align:center"><b>Lembur<b></td>
+    <td width="17%" rowspan="2" bgcolor="#f7fcff" style="text-align:center"><b>Status<br>
     Kehadiran<b></td>
-    <td width="12%" rowspan="2" bgcolor="#f1f1f1" style="text-align:center"><b>Lokasi<br>
+    <td width="12%" rowspan="2" bgcolor="#f7fcff" style="text-align:center"><b>Lokasi<br>
     Absen<b></td>
   </tr>
   <tr>
-    <td width="9%" bgcolor="#f1f1f1" style="text-align:center"><b>Jam<br>Masuk<b></td>
-    <td width="9%" bgcolor="#f1f1f1" style="text-align:center"><b>Jam<br>Pulang<b></td>
-    <td width="9%" bgcolor="#f1f1f1" style="text-align:center"><b>Total<br>Jam Kerja<b></td>
-    <td width="9%" bgcolor="#f1f1f1" style="text-align:center"><b>Masuk<b></td>
-    <td width="9%" bgcolor="#f1f1f1" style="text-align:center"><b>Pulang<b></td>
-    <td width="9%" bgcolor="#f1f1f1" style="text-align:center"><b>Total<br>Lembur<b></td>
+    <td width="9%" bgcolor="#f7fcff" style="text-align:center"><b>Jam<br>Masuk<b></td>
+    <td width="9%" bgcolor="#f7fcff" style="text-align:center"><b>Jam<br>Pulang<b></td>
+    <td width="9%" bgcolor="#f7fcff" style="text-align:center"><b>Total<br>Jam Kerja<b></td>
+    <td width="9%" bgcolor="#f7fcff" style="text-align:center"><b>Masuk<b></td>
+    <td width="9%" bgcolor="#f7fcff" style="text-align:center"><b>Pulang<b></td>
+    <td width="9%" bgcolor="#f7fcff" style="text-align:center"><b>Total<br>Lembur<b></td>
   </tr>
   <tr>
     <td style="text-align: right">1</td>
@@ -193,7 +193,7 @@ function goBack() {
   </tr>
   
 </table>
-<table align="center" class="footnote-1" width="676px" style="margin-top:450px; padding-top:55px; padding-bottom:20px;">
+<table align="center" class="footnote-1" width="650px" style="margin-top:450px; margin-bottom:20px; padding-top:5px; padding-bottom:20px;">
         <thead>
             <tr><th></th>
             <th></th>
@@ -207,7 +207,7 @@ function goBack() {
             
             <tr style="height: 10px"><td></td></tr>
             <tr style=" width:650px;">
-                <td style="text-align: center;">
+                <td style="text-align: left;">
                     <span class="span-qr" style="z-index: 99999">
                         <!--?xml version="1.0" encoding="UTF-8"?-->
 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="40" height="40" viewBox="0 0 40 40"><rect x="0" y="0" width="40" height="40" fill="#ffffff"></rect><g transform="scale(1.905)"><g transform="translate(0,0)"><path fill-rule="evenodd" d="M9 0L9 1L8 1L8 3L9 3L9 4L8 4L8 8L6 8L6 9L5 9L5 10L4 10L4 8L3 8L3 9L2 9L2 8L0 8L0 9L2 9L2 10L3 10L3 12L4 12L4 11L7 11L7 10L8 10L8 8L9 8L9 11L8 11L8 12L6 12L6 13L8 13L8 15L9 15L9 16L10 16L10 17L11 17L11 16L12 16L12 17L13 17L13 18L12 18L12 19L10 19L10 20L11 20L11 21L12 21L12 20L13 20L13 19L14 19L14 21L15 21L15 18L16 18L16 19L17 19L17 20L19 20L19 18L20 18L20 20L21 20L21 17L20 17L20 16L21 16L21 14L20 14L20 15L19 15L19 16L18 16L18 13L19 13L19 12L20 12L20 13L21 13L21 12L20 12L20 11L19 11L19 10L21 10L21 9L20 9L20 8L18 8L18 9L17 9L17 8L14 8L14 9L13 9L13 8L12 8L12 7L13 7L13 6L12 6L12 7L11 7L11 6L10 6L10 7L9 7L9 4L10 4L10 0ZM12 0L12 1L11 1L11 4L12 4L12 5L13 5L13 2L12 2L12 1L13 1L13 0ZM6 9L6 10L7 10L7 9ZM11 9L11 10L10 10L10 11L9 11L9 12L8 12L8 13L9 13L9 15L10 15L10 16L11 16L11 14L13 14L13 15L12 15L12 16L13 16L13 17L17 17L17 19L18 19L18 18L19 18L19 17L18 17L18 16L17 16L17 15L16 15L16 14L13 14L13 13L16 13L16 12L14 12L14 10L15 10L15 11L17 11L17 13L18 13L18 12L19 12L19 11L18 11L18 10L19 10L19 9L18 9L18 10L15 10L15 9L14 9L14 10L13 10L13 9ZM12 10L12 11L11 11L11 12L12 12L12 13L13 13L13 12L12 12L12 11L13 11L13 10ZM1 11L1 12L0 12L0 13L1 13L1 12L2 12L2 11ZM9 12L9 13L10 13L10 12ZM14 15L14 16L16 16L16 15ZM8 17L8 18L9 18L9 17ZM8 19L8 21L9 21L9 19ZM0 0L0 7L7 7L7 0ZM1 1L1 6L6 6L6 1ZM2 2L2 5L5 5L5 2ZM14 0L14 7L21 7L21 0ZM15 1L15 6L20 6L20 1ZM16 2L16 5L19 5L19 2ZM0 14L0 21L7 21L7 14ZM1 15L1 20L6 20L6 15ZM2 16L2 19L5 19L5 16Z" fill="#000000"></path></g></g></svg>
@@ -215,7 +215,7 @@ function goBack() {
                     </span>
                 </td>
                 <td>&nbsp;</td>
-                <td colspan="2" valign="center" style="position: relative;top:-5px;text-align:left; padding-left:10px; padding-top:10px;">
+               <td colspan="2" valign="left" style="text-align: center">
                     <span style="text-align:justify; line-height:1.5; font-family: Roboto; font-size: 11px; "><i>Sesuai
                             dengan ketentuan Peraturan Perusahaan yang berlaku, Perusahaan mengatur
                             bahwa
@@ -229,12 +229,15 @@ function goBack() {
                 <td colspan="4" style="border-bottom: 1px solid #ccc;"></td>
             </tr>
             <tr style=" width:650px;">
-              <td colspan="4" style="text-align: left;"><span style="text-align:justify; line-height:1.5; font-family: Roboto; font-size: 11px; "><i>Document Printed By : Muhammad Nasrullah, S.Kom - 16/11/2025 - 20:39 WITA - Halaman 1 dari 100</i></span></td>
+              <td colspan="4" style="text-align: center;"><span style="text-align:justify; line-height:1.5; font-family: Roboto; font-size: 11px; "><i>Document Printed By : Muhammad Nasrullah, S.Kom - 16/11/2025 - 20:39 WITA - Halaman 1 dari 1</i></span></td>
+            </tr>
+			<tr style=" width:650px;">
+              <td colspan="4" style="text-align: center;"><span style="text-align:justify; line-height:1.5; font-family: Roboto; font-size: 11px; ">&nbsp;</span></td>
             </tr>
 
         </tbody>
     </table>
-  <img src="../assets/img/footer-aksikode-release.png" style="width:676px; display:block; margin-left:auto; margin-top: 30px; margin-right:auto;margin-bottom:20px;padding-bottom:30px" />
+  
 
   
 								</div>
